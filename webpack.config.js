594472969220a01b90e0c83a2757067ce2512386
Damir <damir@yusipov.ru>
2020-10-04 11:51:57 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const webpack = require('webpack');
 const path = require('path')
 const pkg = require('./package.json');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = function(options) {
     if (!options) options = {};
 
     return {
         mode: "production",
-        entry: path.join(__dirname, "src/index.ts"),
-
+        entry: {
+            "magx": path.join(__dirname, "src/index.ts"),
+            "magx.min": path.join(__dirname, "src/index.ts"),
+        },
         output: {
             path: path.join(__dirname, "./dist/"),
-            filename: (options.production)
-                ? "magx.js"
-                : "magx.dev.js",
+            filename: "[name].js",
 
             globalObject: "self || this", // compatibility with Web Workers.
             libraryTarget: "umd",
@@ -30,7 +31,7 @@ module.exports = function(options) {
 
         plugins: [
             new webpack.BannerPlugin({ banner: `magx.js@${pkg.version}` }),
-            // new webpack.DefinePlugin({ 'process.env.VERSION': JSON.stringify(pkg.version) }),
+            // new webpack.optimize.UglifyJsPlugin({ include: /\.min\.js$/, minimize: true})
         ],
 
         // hack: react-native is not used for the distribution build
@@ -38,7 +39,10 @@ module.exports = function(options) {
         },
 
         optimization: {
-            minimize: (options.production || false) // only minimize on production
+            minimize: true,
+            minimizer: [new UglifyJsPlugin({
+                include: /\.min\.js$/
+            })]
         },
 
         resolve: {
